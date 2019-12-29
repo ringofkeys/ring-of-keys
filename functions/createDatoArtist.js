a064d8c55af62d9e93b89bf5e2d5f760b77bb957
franknoirot <frank@franknoirot.co>
2019-12-29 05:48:45 -0500

    Typo in Netlify Fn...

--- functions/createDatoArtist.js
+++ functions/createDatoArtist.js
@@ -1,5 +1,5 @@
 exports.handler = async event => {
-    cosnt subject = event.queryStringParameters.name || 'World'
+    const subject = event.queryStringParameters.name || 'World'
     return {
         statusCode: 200,
         body: `Hello ${subject}!`
