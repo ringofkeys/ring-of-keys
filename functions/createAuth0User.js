93deb11e85e8785ee9d5b007690039887aba5833
franknoirot <frank@franknoirot.co>
2020-02-02 11:55:03 -0500

    debugging Auth0 scopes

--- functions/createAuth0User.js
+++ functions/createAuth0User.js
@@ -56,7 +56,7 @@ function getAuth0Token() {
       client_secret: `${process.env.AUTH_SECRET}`,
       audience: `https://${ process.env.AUTH0_DOMAIN }/api/v2/`,
       grant_type: "client_credentials",
-      scope: 'create:users, read:users',
+      scope: 'create:users read:users',
     }),
   }
 
