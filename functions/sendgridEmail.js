67bbf1f4bb47403a06ae43177c88f298b6cd9403
franknoirot <frank@franknoirot.co>
2020-02-11 13:27:36 -0500

    replacing new lines with spaces

--- functions/sendgridEmail.js
+++ functions/sendgridEmail.js
@@ -10,7 +10,7 @@ sgMail.setApiKey(process.env.SENDGRID_KEY)
 exports.handler = async (event) => {
     console.log('event.body = ', event.body)
 
-    event.body = event.body.replace(/(\r\n|\n|\r)/gm,'<br/>')
+    event.body = event.body.replace(/\r\n|\n|\r/gm,' ')
     console.log('escapes event.body = ', event.body)
 
     const data = JSON.parse(event.body)
