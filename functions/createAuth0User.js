a69389b834740852bc5f6171827986334a7a044a
franknoirot <frank@franknoirot.co>
2020-02-12 02:24:47 -0500

    adding myself as bcc to new user email, duh!

--- functions/createAuth0User.js
+++ functions/createAuth0User.js
@@ -68,7 +68,7 @@ exports.handler = async (event) => {
     }
 
     const emailSendResponse = await sendWelcomeEmail(userData.email, userData.name, resetPasswordResponse.ticket).catch(err => JSON.stringify(err))
-    console.log('Email Sent: ', emailSendResponse) 
+    console.log('Email Sent') 
     
     return {
       statusCode: 201,
@@ -159,6 +159,7 @@ async function sendWelcomeEmail(email, name, pwdResetUrl) {
   const msg = {
     to: email,
     from: 'info@ringofkeys.org',
+    bcc: [{ email: 'frank.ringofkeys@gmail.com' }],
     subject: 'Welcome to Ring of Keys',
     text: 'You can set up your password at '+ pwdResetUrl,
     html:  `<table border="0" cellpadding="0" cellspacing="0" width="100%" class="templateContainer" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;border: 0;max-width: 600px !important;">
