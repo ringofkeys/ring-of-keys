3c5f6b1f109187f4565e2f85adced7249517a479
franknoirot <frank@franknoirot.co>
2020-02-16 10:45:05 -0500

    added focus to Carosuel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -6,17 +6,9 @@ import './carousel.css'
 
 
 const Carousel = ({ recordType, itemList, classNames }) => {
-    // const [currIndex, setIndex] = useState(0)
-    // const [numCardsVisible, setNumCardsVisible] = useState(3)
-
-    // useEffect(() => window.addEventListener('resize', resize), [])
-
-    // function resize() {
-    //     setNumCardsVisible((window.innerWidth < 700) ? 1 : 3)
-    // }
 
     return (
-        <div className={`carousel ${ classNames.length > 0 ? classNames.join(' ') : '' }`}>
+        <div ariaRole='region' tabIndex='0' className={`carousel ${ classNames.length > 0 ? classNames.join(' ') : '' }`}>
             <div className='carousel_inner'>
             {
                 itemList.map(({ node }) => (
