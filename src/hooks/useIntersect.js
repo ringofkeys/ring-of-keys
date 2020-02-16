8a1f64e2acad9a52929c81b44aad85000db973a5
franknoirot <frank@franknoirot.co>
2020-02-16 15:51:46 -0500

    made preview popup stay until dismissed, larger font

--- src/hooks/useIntersect.js
+++ src/hooks/useIntersect.js
@@ -1,11 +1,10 @@
 import { useState, useRef, useEffect } from 'react'
-import { isBrowser } from '../utils/auth'
 
 export const useIntersect = ({ root = null, rootMargin, threshold = 0 }) => {
     const [entry, updateEntry] = useState({})
     const [node, setNode] = useState(null)
 
-    const observer = isBrowser ? useRef(
+    const observer = useRef(
         new window.IntersectionObserver(([entry]) => {
             // console.log('observed!', entry)
             updateEntry(entry)
@@ -14,17 +13,15 @@ export const useIntersect = ({ root = null, rootMargin, threshold = 0 }) => {
             rootMargin,
             threshold
         })
-    ) : null
+    )
 
     useEffect(() => {
-        if (observer) {
-            const { current: currentObserver } = observer
-            currentObserver.disconnect()
+        const { current: currentObserver } = observer
+        currentObserver.disconnect()
 
-            if (node) currentObserver.observe(node)
-            // console.log('now observing node =', node)
-            return () => currentObserver.disconnect()
-        }
+        if (node) currentObserver.observe(node)
+        // console.log('now observing node =', node)
+        return () => currentObserver.disconnect()
     }, [node])
 
     return [setNode, entry]
--- src/templates/key.js
+++ src/templates/key.js
@@ -358,12 +358,29 @@ export default ({ data }) => {
                 )}
             </section>
         </Layout>
-        { hasSubmitted && <div className='preview-message'>
-            <p>
-                You're viewing a preview of your new profile content. We're rebuilding the site now with your edits, and you
-                should see them across the site within a few minutes.
-            </p>
-        </div>}
+        { hasSubmitted && <div className={`preview-message`}>
+            <div className='wrapper'>
+                <p>
+                    You're viewing a preview of your new profile content. We're rebuilding the site now with your edits, and you
+                    should see them across the site within a few minutes.
+                </p>
+                <button className='btn_close' onClick={ e => {
+                        console.log('target = ', e.target)
+                        let elem = e.target
+                        while (!elem.classList.contains('preview-message') && elem.parentElement) {
+                            elem = elem.parentElement
+                        }
+
+                        elem.classList.add('closed')
+                    }}>
+                    <span className='visually-hidden'>Close</span>
+                    <svg viewBox='0 0 5 5'>
+                        <path d='M 1 1 l 3 3' stroke='black' />
+                        <path d='M 1 4 l 3 -3' stroke='black' />
+                    </svg>
+                </button>
+            </div>
+        </div> }
         <MessagePopup isOpen={ isMessageOpen } artistId={ id } onClose={ () => setMessageOpen(false) } />
         {/* <Popup isOpen={isMessageOpen} onClose={() => setMessageOpen(false)} >
             <h2>Messaging is coming soon!</h2>
