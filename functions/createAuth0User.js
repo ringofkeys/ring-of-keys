e53e43168329c75a90c97843dbd426dc01832365
franknoirot <frank@franknoirot.co>
2020-04-29 18:20:13 -0400

    seeing if the build trigger is the problem

--- functions/createAuth0User.js
+++ functions/createAuth0User.js
@@ -62,16 +62,16 @@ exports.handler = async (event) => {
         }
     }
 
-    console.log('testing we get to just before the deploy trigger is fired')
+    // console.log('testing we get to just before the deploy trigger is fired')
 
-    const buildTriggerId = '8003'
-    client.buildTriggers.trigger(buildTriggerId)
-        .then(() => {
-            console.log('Done!');
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+    // const buildTriggerId = '8003'
+    // client.buildTriggers.trigger(buildTriggerId)
+    //     .then(() => {
+    //         console.log('Done!');
+    //     })
+    //     .catch((error) => {
+    //         console.error(error);
+    //     });
     
 
 
