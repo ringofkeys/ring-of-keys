b6aa402c720bdbe5176c497d5f7c99d3db14d074
Frank Noirot <frank@franknoirot.co>
2021-02-01 23:11:12 -0500

    reconfigured Stripe Products to be broken out to better support Customer Portal.

--- src/utils/stripeProducts.js
+++ src/utils/stripeProducts.js
@@ -16,26 +16,26 @@ const products = [
         label: '🤝 Multiplicity',
         annual: {
             text: '$50/yr',
-            keyship_id: 'price_1IGELyFPVymKtzoPv8fyhaYd',
-            sponsorship_id: 'price_1IGEPSFPVymKtzoPOih7H2WK',
+            keyship_id: 'price_1IGG64FPVymKtzoPnxuzwmmP',
+            sponsorship_id: 'price_1IGG0mFPVymKtzoPwYxiXFqC',
         },
         monthly: {
             text: '$5/mo',
-            keyship_id: 'price_1IGELyFPVymKtzoPhq0ak1SM',
-            sponsorship_id: 'price_1IGEPSFPVymKtzoPA9aze6rn',
+            keyship_id: 'price_1IGG64FPVymKtzoP1XwriwkQ',
+            sponsorship_id: 'price_1IGG0mFPVymKtzoP6SsOpcER',
         },
     },
     { 
         label: '🔑 Possibility',
         annual: {
             text: '$100/yr',
-            keyship_id: 'price_1IGELyFPVymKtzoPhyXTm3Ix',
-            sponsorship_id: 'price_1IGEPSFPVymKtzoP4Kx0gvh8',
+            keyship_id: 'price_1IGG7vFPVymKtzoPRYHAjlqC',
+            sponsorship_id: 'price_1IGG94FPVymKtzoP1MKCEOyx',
         },
         monthly: {
             text: '$10/mo',
-            keyship_id: 'price_1IGELyFPVymKtzoPUbJPzvs7',
-            sponsorship_id: 'price_1IGEPSFPVymKtzoPSUyFPunw',
+            keyship_id: 'price_1IGG7vFPVymKtzoPf95kWru4',
+            sponsorship_id: 'price_1IGG94FPVymKtzoP4of1VP6o',
         },
     },
 ]
