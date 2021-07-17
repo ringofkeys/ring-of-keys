8b147e1b81d1e1ce21ab4ae8186c84599a246bd6
Frank Noirot <frankjohnson1993@gmail.com>
2021-07-16 23:53:29 -0400

    initial commit: switched dependencies, updated page and style directories, beginning Next implementation.

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Ring of Keys","short_name":"RoK","start_url":"/","background_color":"#263e64","theme_color":"#e9bfb2","display":"minimal-ui","icon":"src/images/rok_favicon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"0813789f311aabd5580e6316b448e8b9"},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/sync-requires.js
@@ -0,0 +1,27 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\pages\\404.js"))),
+  "component---src-pages-apply-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\pages\\apply.js"))),
+  "component---src-pages-callback-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\pages\\callback.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\pages\\contact.js"))),
+  "component---src-pages-dashboard-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\pages\\dashboard.js"))),
+  "component---src-pages-directory-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\pages\\directory.js"))),
+  "component---src-pages-donate-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\pages\\donate.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\pages\\index.js"))),
+  "component---src-pages-news-index-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\pages\\news\\index.js"))),
+  "component---src-pages-privacy-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\pages\\privacy.js"))),
+  "component---src-pages-resources-index-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\pages\\resources\\index.js"))),
+  "component---src-pages-terms-e-communications-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\pages\\terms-e-communications.js"))),
+  "component---src-pages-terms-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\pages\\terms.js"))),
+  "component---src-templates-event-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\templates\\event.js"))),
+  "component---src-templates-key-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\templates\\key.js"))),
+  "component---src-templates-page-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\templates\\page.js"))),
+  "component---src-templates-resource-type-js": hot(preferDefault(require("C:\\Users\\frank\\Code\\rok\\src\\templates\\resourceType.js")))
+}
+
