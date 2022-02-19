8ae1411d246c68f7bfa5d23d17d917f8b8269ecd
Frank Johnson <frankjohnson1993@gmail.com>
2022-02-19 10:19:29 -0500

    Cleanup repo, regularize dirs with _old convention

--- src/pages/_old/directory.js
+++ src/pages/_old/directory.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react" 
 import { graphql } from "gatsby"
 import Fuse from "fuse.js"
 import { useFormik } from "formik"
