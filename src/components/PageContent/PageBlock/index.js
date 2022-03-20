b72d09792f047791605f1f92251fcb3b92422104
Frank Johnson <frankjohnson1993@gmail.com>
2022-03-20 00:03:46 -0400

    Rough rendering of home page hero

--- src/components/PageContent/PageBlock/index.js
+++ src/components/PageContent/PageBlock/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import BasicBlock from './BasicBlock';
 import Shortcode from './Shortcode';
+import Hero from './Hero';
 
 export default function PageBlock(props) {
     if (!props.__typename) return <></>
@@ -10,6 +11,8 @@ export default function PageBlock(props) {
             return <BasicBlock {...props} />
         case "ShortcodeRecord":
             return <Shortcode {...props} />
+        case "HeroRecord":
+            return <Hero {...props} />
         default:
             console.log("Unsupported page block type in use", props)
             return <></>
