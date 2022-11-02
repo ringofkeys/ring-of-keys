778f0adba5f436753cee9fb3d8f8bc4737579e40
Frank Johnson <frankjohnson1993@gmail.com>
2022-11-02 02:08:50 -0400

    bugfix: remove redirects on "-2" pages

--- next.config.js
+++ next.config.js
@@ -4,17 +4,6 @@ module.exports = {
         styledComponents: true,
     },
     async redirects() {
-        return [
-          {
-            source: '/directory',
-            destination: '/directory-2',
-            permanent: true,
-          },
-          {
-            source: '/resources',
-            destination: '/resources-2',
-            permanent: true,
-          },
-        ]
+        return []
     },
 }
