7395bfc75f948832c2679462b86458d890883784
Frank Johnson <frankjohnson1993@gmail.com>
2023-06-11 12:34:47 -0400

    Add Sentry, update Tay email, update default SEO

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
-module.exports = {
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const sentryPluginOptions = {
+    org: 'ring-of-keys',
+    project: 'ringofkeys-site',
+    silent: true,
+}
+
+const nextConfig = {
     swcMinify: true,
     compiler: {
         styledComponents: true,
+    },
+    sentry: {
+        hideSourceMaps: true,
     },
     async redirects() {
         return []
     },
 }
+
+module.exports = withSentryConfig(nextConfig, sentryPluginOptions)
