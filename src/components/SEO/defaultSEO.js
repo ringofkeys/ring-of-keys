4255abd36e6912fb668966483dfb09b837cd27fc
Frank Noirot <frank@kittycad.io>
2024-06-29 17:31:53 -0400

    Remove only instance of "gender non-conforming"

--- src/components/SEO/defaultSEO.js
+++ src/components/SEO/defaultSEO.js
@@ -1,7 +1,7 @@
 export const defaultSEO = {
     title: "LGBTQ Musical Theatre Network",
     description:
-        "Ring of Keys is a 501(c)3 artist service organization of queer women, trans, and gender non-conforming artists in musical theatre.",
+        "Ring of Keys is a 501(c)3 artist service organization of queer women, trans, and nonbinary artists in musical theatre.",
     image: {
         url: "https://www.datocms-assets.com/20110/1635698353-rok-metacard.jpg",
     },
