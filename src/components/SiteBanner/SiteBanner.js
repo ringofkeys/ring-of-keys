fffedcb93a1e1954e0d016a8ac02f0ddf2d5bcce
Amanda_Ortiz <contactingaortiz@gmail.com>
2025-08-29 16:51:24 -0500

    banner updates

--- src/components/SiteBanner/SiteBanner.js
+++ src/components/SiteBanner/SiteBanner.js
@@ -10,7 +10,7 @@ const SITE_BANNER_UPDATE_KEY = "rok-site-banner-last-updated"
 const SITE_BANNER_LAST_UPDATED = 1688051553846
 
 export default function SiteBanner({
-    textContent = `Queering the Canon: Live at Joe’s Pub will be available for streaming July 15, 2025! <a href="https://distrokid.com/hyperfollow/ringofkeys/queering-the-canon-live-at-joes-pub?utm_campaign=website&utm_medium=Email+&utm_source=SendGrid" target="_blank" rel="noopener noreferrer">Pre-save today!</a>`,
+    textContent = `Queering the Canon: Live at Joe’s Pub is now streaming! <a href="https://open.spotify.com/album/11xd4dGMlNieSkwsDA1mSF" target="_blank" rel="noopener noreferrer">Listen on Spotify</a> or wherever you get your music.`,
     ctaUrl = "/donate",
     ctaText = "Donate",
     showCta = true,
