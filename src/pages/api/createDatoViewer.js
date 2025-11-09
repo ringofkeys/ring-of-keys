e02868e0ff64006f8e61ea9f3257506a221c1394
Frank Noirot <frankjohnson1993@gmail.com>
2025-11-08 20:01:23 -0500

    Replace deprecate DatoCMS client with newer one

--- src/pages/api/createDatoViewer.js
+++ src/pages/api/createDatoViewer.js
@@ -1,19 +1,19 @@
-const { SiteClient } = require("datocms-client")
+const { SiteClient } = require("@datocms/cma-client")
 const client = new SiteClient(process.env.DATO_CONTENT_TOKEN)
 
 async function handler(req, res) {
-    const data = JSON.parse(req.body)
-    data.itemType = "194216"
+  const data = JSON.parse(req.body)
+  data.itemType = "194216"
 
-    try {
-        const newViewerRes = await client.items.create(data)
+  try {
+    const newViewerRes = await client.items.create(data)
 
-        res.status(201).json(newViewerRes)
-    } catch (err) {
-        console.log("An error was found!", err)
+    res.status(201).json(newViewerRes)
+  } catch (err) {
+    console.log("An error was found!", err)
 
-        res.status(500).json(err)
-    }
+    res.status(500).json(err)
+  }
 }
 
 export default handler
